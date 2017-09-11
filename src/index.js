daace0a88fd59b1a0ee513a19ef65cc0bdd53fba
Andy Edwards <jedwards@fastmail.com>
2017-09-11 01:18:24 -0500

    refactor(index): change component name

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import warning from 'warning'
 import invariant from 'invariant'
 import matchPath from 'react-router/matchPath'
 
-/**
- * The public API for rendering the first <Route> that matches.
- */
-class Switch extends React.Component {
+class TransitionSwitch extends React.Component {
   static contextTypes = {
     router: PropTypes.shape({
       route: PropTypes.object.isRequired
@@ -22,19 +19,19 @@ class Switch extends React.Component {
   componentWillMount() {
     invariant(
       this.context.router,
-      'You should not use <Switch> outside a <Router>'
+      'You should not use <TransitionSwitch> outside a <Router>'
     )
   }
 
   componentWillReceiveProps(nextProps) {
     warning(
       !(nextProps.location && !this.props.location),
-      '<Switch> elements should not change from uncontrolled to controlled (or vice versa). You initially used no "location" prop and then provided one on a subsequent render.'
+      '<TransitionSwitch> elements should not change from uncontrolled to controlled (or vice versa). You initially used no "location" prop and then provided one on a subsequent render.'
     )
 
     warning(
       !(!nextProps.location && this.props.location),
-      '<Switch> elements should not change from controlled to uncontrolled (or vice versa). You provided a "location" prop initially but omitted it on a subsequent render.'
+      '<TransitionSwitch> elements should not change from controlled to uncontrolled (or vice versa). You provided a "location" prop initially but omitted it on a subsequent render.'
     )
   }
 
@@ -71,4 +68,4 @@ class Switch extends React.Component {
   }
 }
 
-export default Switch
+export default TransitionSwitch
